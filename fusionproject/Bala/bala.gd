747c51953c737e79a9a8ffc9f16ad4bd25ba2d3d
emanuel886 <emanuelarthurasilva@gmail.com>
2026-04-05 08:25:58 -0300

    bala

--- fusionproject/Bala/bala.gd
+++ fusionproject/Bala/bala.gd
@@ -4,12 +4,16 @@ var dir := Vector2.ZERO
 var speed : float = 0.0
 var damage : float = 0.0
 var iniciate := false
+var meu_grupo : String
+var alvo : String
 
-func iniciar(s: float, d: float, direction: Vector2):
+func iniciar(s: float, d: float, direction: Vector2,m,a):
 	speed = s
 	damage = d
 	dir = direction.normalized()
 	iniciate = true
+	meu_grupo = m
+	alvo = a
 
 
 func _physics_process(delta: float) -> void:
@@ -17,3 +21,7 @@ func _physics_process(delta: float) -> void:
 		return
 	
 	global_position += dir * speed * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
