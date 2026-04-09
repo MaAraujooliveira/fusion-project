a344e56a2bf9542b3d805af653740fca0f0ee2ba
emanuel886 <emanuelarthurasilva@gmail.com>
2026-04-08 21:22:01 -0300

    vadasdasdasd

--- fusionproject/Base/base.gd
+++ fusionproject/Base/base.gd
@@ -4,7 +4,8 @@ class_name Base
 @export var hp := 100
 @export var max_hp := 100
 @export var coin_scene : PackedScene
-@export var spawn_radius := 200.0
+@export var spawn_radius := 2000.0
+@export var min_spawn_radius := 1000.0
 @export var coin_amount := 5
 @export var spawn_time := 2.0
 
@@ -38,12 +39,10 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func spawn_coins():
 	for i in range(coin_amount):
-		var angle = randf_range(0, TAU)
-		var offset = Vector2(cos(angle), sin(angle)) * spawn_radius
 		
 		var coin = coin_scene.instantiate()
 		get_tree().current_scene.add_child(coin)
-		coin.global_position = global_position + offset
+		coin.global_position = get_spawn_position()
 		
 		# 💥 EFEITO BOUNCE
 		coin.scale = Vector2.ZERO
@@ -62,3 +61,10 @@ func _draw():
 		var radius = shape.radius
 		
 		draw_circle(Vector2.ZERO, radius, Color(0.0, 1.0, 0.0, 1.0),false,50) # verde transparente
+
+func get_spawn_position():
+	var angle = randf() * TAU
+	var dir = Vector2(cos(angle), sin(angle))
+	var distance = randf() * spawn_radius
+	
+	return  dir * distance
